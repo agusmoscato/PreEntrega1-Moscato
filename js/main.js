0ce4673c6fb2f2cfa108be04b055d6706da20087
Agustin Moscato <agustinmoscato369@gmail.com>
2023-01-04 15:41:56 -0300

    correcciones

--- js/main.js
+++ js/main.js
@@ -1,19 +1,29 @@
-let cantPersonas = prompt("Bienvenido! Cuantas personas quiere cargar hoy?")
-let sumaDeEdades = 0
-for (let i = 1; i <= cantPersonas; i++){
-    let nombre = prompt("Ingresar el nombre de la persona nº " + i)
-    let edad = parseInt(prompt("Ingresar edad de " + nombre))
-    sumaDeEdades = sumaDeEdades + edad;
-    if (edad >= 18) {
-        alert("Cargaste una persona mayor de edad")
-    } else {
-        alert("Cargaste una persona que no es mayor de edad")
+function PrimeraEntrega() {
+    let cantPersonas = parseInt(prompt("Bienvenido! Cuantas personas quiere cargar hoy?"))
+    let sumaDeEdades = 0
+    let edad = 0
+
+    for (let i = 1; i <= cantPersonas; i++) {
+        let nombre = prompt("Ingresar el nombre de la persona nº " + i)
+        do {
+            edad = parseInt(prompt("Ingresar edad de " + nombre))
+        } while (isNaN(edad));
+        sumaDeEdades = sumaDeEdades + edad;
+        if (edad >= 18) {
+            alert("Cargaste una persona mayor de edad")
+        } else {
+            alert("Cargaste una persona que no es mayor de edad")
+        }
+    }
+
+    let promDeEdades = 0
+    if (cantPersonas > 0) {
+        promDeEdades = sumaDeEdades / cantPersonas;
+        alert("El promedio de edad de las personas cargadas es de: " + promDeEdades + " años")
     }
 }
 
-let promDeEdades = sumaDeEdades / cantPersonas;
-
-alert("El promedio de edad de las personas cargadas es de: " + promDeEdades + " años")
+PrimeraEntrega();
 
 
 
