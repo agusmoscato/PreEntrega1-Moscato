573169004ca533cae1957071f3554c3874b32c24
Agustin Moscato <agustinmoscato369@gmail.com>
2023-01-23 16:38:21 -0300

    Segunda Entrega

--- js/main.js
+++ js/main.js
@@ -1,29 +1,55 @@
-function PrimeraEntrega() {
-    let cantPersonas = parseInt(prompt("Bienvenido! Cuantas personas quiere cargar hoy?"))
-    let sumaDeEdades = 0
-    let edad = 0
-
-    for (let i = 1; i <= cantPersonas; i++) {
-        let nombre = prompt("Ingresar el nombre de la persona nº " + i)
-        do {
-            edad = parseInt(prompt("Ingresar edad de " + nombre))
-        } while (isNaN(edad));
-        sumaDeEdades = sumaDeEdades + edad;
-        if (edad >= 18) {
-            alert("Cargaste una persona mayor de edad")
-        } else {
-            alert("Cargaste una persona que no es mayor de edad")
+function SegundaEntrega() {
+    /* Constructor obejeto "persona" */
+    class Persona {
+        constructor(nombre, edad) {
+            this.nombre = nombre;
+            this.edad = edad;
         }
     }
 
-    let promDeEdades = 0
-    if (cantPersonas > 0) {
-        promDeEdades = sumaDeEdades / cantPersonas;
-        alert("El promedio de edad de las personas cargadas es de: " + promDeEdades + " años")
+    /* Inicio del programa */
+    let cantPersonas = parseInt(prompt("Bienvenido! Cuantas personas quiere cargar hoy?"))
+    let edad = 0
+    const arrayPersonas = [];
+
+
+    function pedirEdad(cantPersonas) {
+        for (let i = 1; i <= cantPersonas; i++) {
+            let nombre = prompt("Ingresar el nombre de la persona nº " + i)
+            do {
+                edad = parseInt(prompt("Ingresar edad de " + nombre))
+            } while (isNaN(edad));
+            arrayPersonas[i - 1] = new Persona(nombre, edad);
+            preguntarSiEsMayor(nombre, edad);
+        }
     }
+    function preguntarSiEsMayor(nombre, edad) {
+        if (edad >= 18) {
+            alert(nombre + " es mayor de edad")
+        } else {
+            alert(nombre + " no es mayor de edad")
+        }
+    }
+
+    function promedioEdades(arrayPersonas, cantPersonas) {
+
+        if (cantPersonas) {
+            let sumaEdades = arrayPersonas.reduce((total, arrayPersonas) => {
+                return total + arrayPersonas.edad;
+            }, 0);
+
+            let promedioEdades = sumaEdades / arrayPersonas.length;
+            console.log(promedioEdades);
+            alert("El promedio de edad de las personas cargadas es de: " + promedioEdades + " años")
+        }
+    }
+
+
+    pedirEdad(cantPersonas);
+    promedioEdades(arrayPersonas, cantPersonas);
+
+    console.log(arrayPersonas)
 }
 
-PrimeraEntrega();
-
-
-
+/* Ejecutar Programa */
+SegundaEntrega();
